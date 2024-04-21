1e723214d184c6dd3c0dea025fb87c4def5367a0
Денис Суржиков <pinkideathman@gmail.com>
2024-04-21 22:08:52 +0300

    Updated master

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+//добавить необходимые заголовочные файлы
+int is_leap(int year);
+int closest_leap_year(int year);
+void fill_list_of_years(int* allocated); // Упрощение, не принимать за истину
+
+
+int main(void) {
+    // КОД СЮДА
+    return 0;
+}
